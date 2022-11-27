02e783383b9030dff3c099c5bf99ba65662639ed
sollapse <sollapse@gmail.com>
2022-11-27 09:06:05 -0600

    Add files via upload

---
+++ mpk2_autofdbk.lua
@@ -0,0 +1,259 @@
+ardour {
+    ["type"] = "EditorHook",
+    name = "MPK2 Auto Feedback",
+    author = "sollapse",
+    license = "MIT",
+    description = "Automatically provide pad feedback and bank switching on MPK2 series controllers from tagged MIDI track selected. \n\nMPK2 series SysEx information provided by Nick Smith @ https://github.com/nsmith-/mpk2"
+}
+
+function signals()
+    s = LuaSignal.Set()
+    s:add({
+        [LuaSignal.Change] = true
+    })
+    return s
+end
+
+function factory(params)
+    -- globals
+    padfdbk = nil -- Pad feedback MIDI track
+    parser = nil -- Raw MIDI parser for SysEx
+    sel = nil -- editor selection object
+    padfdbk_name = "" -- Pad feedback MIDI track name
+    mpk2_out = "" -- MPK2 hardware out port name
+    mpk2_padchn = 0 -- MPK2 pads channel
+    mpk2_model = 0 -- SysEx identifier for controller model (MPK225 = 0x23, MPK249 = 0x24, MPK261 = 0x25)
+    noteoff_buf = "" -- buffer for all note off messages 
+    multi_sel = false
+    configured = false
+    debug = true -- print debug messages
+
+    function parseComments(str)
+        -- check for brackets
+        b, e = string.find(str, "%b[]")
+        if b > 0 and e > 0 then
+            local s = string.sub(str, b + 1, e - 1)
+            return s:upper()
+        else
+            return ""
+        end
+    end
+
+    -- helper function to send MIDI
+    function tx_midi(syx, len, safesend)
+        for b = 1, len do
+            -- parse message to C/C++ uint8_t* array (Validate message correctness. This
+            -- also returns C/C++ uint8_t* array for direct use with write_immediate_event.)
+            if parser:process_byte(syx:byte(b)) then
+                padfdbk:write_immediate_event(Evoral.EventType.MIDI_EVENT, parser:buffer_size(), parser:midi_buffer())
+                if safesend then
+                    -- Slow things down a bit to ensure that no messages as lost.
+                    -- Physical MIDI is sent at 31.25kBaud.
+                    -- Every message is sent as 10bit message on the wire,
+                    -- so every MIDI byte needs 320usec.
+                    ARDOUR.LuaAPI.usleep(400 * parser:buffer_size())
+                end
+            end
+        end
+    end
+
+    return function(signal, ref, ...)
+        -- build config filename
+        cfgname = ARDOUR.LuaAPI.build_filename(Session:path(), "mpk2.cfg")
+
+        function chkConfig()
+            -- attempt to read config file and load variables
+            if ARDOUR.LuaAPI.file_test(cfgname, ARDOUR.LuaAPI.FileTest.Exists) then
+                cfgfile = io.open(cfgname, "r+")
+                io.input(cfgfile)
+
+                -- load variables
+                local r = io.read()
+
+                local vars = {}
+                for v in r:gmatch("([^,]+)") do
+                    table.insert(vars, v)
+                end
+
+                if #vars == 4 then
+                    mpk2_out = vars[1]
+                    mpk2_model = tonumber(vars[2], 16)
+                    mpk2_padchn = tonumber(vars[3])
+                    padfdbk_name = vars[4]
+
+                    -- get feedback track object
+                    local r = Session:route_by_name(padfdbk_name)
+
+                    if not r:to_track():to_midi_track():isnil() then
+                        padfdbk = r:to_track():to_midi_track()
+                        configured = true
+                        io.close(cfgfile)
+                    end
+                else
+                    io.close(cfgfile)
+                end
+
+                if configured then
+                    -- Set raw midi parser
+                    parser = ARDOUR.RawMidiParser()
+
+                    -- fill all notes off buffer
+                    for i = 0, 127 do
+                        noteoff_buf = noteoff_buf ..
+                                          string.char(
+                                (0x80 & 0xF0) + tonumber(string.format("%x", mpk2_padchn - 1), 16),
+                                tonumber(string.format("%x", i), 16), 0x00)
+                    end
+                else
+                    if debug then
+                        print("MPK2 Auto Feedback not configured.")
+                    end
+                    return
+                end
+            end
+        end
+
+        function configFeedback()
+            if debug then
+                print("Multiple selections: " .. tostring(multi_sel) .. " Size: " ..
+                          tostring(sel.tracks:routelist():size()))
+            end
+            -- disconnect here for multiple MIDI selections
+            if multi_sel then
+                -- disconnect old
+                padfdbk:input():midi(0):disconnect_all()
+            end
+
+            if #padfdbk:comment() > 0 then
+                playfdbk = parseComments(padfdbk:comment())
+                if playfdbk == "PLAY" then
+                    goto process
+                else
+                    goto skip
+                end
+            else
+                playfdbk = ""
+                goto skip
+            end
+
+            ::process::
+            -- send all notes off
+            tx_midi(noteoff_buf, #noteoff_buf, false)
+
+            -- check for valid MIDI track(s)
+            for t in Session:get_tracks():iter() do
+                local miditrk = t:to_track():to_midi_track()
+
+                if not miditrk:isnil() and not padfdbk:isnil() and miditrk:is_selected() and #miditrk:comment() > 0 then
+                    -- don't make change on feedback track
+                    if not miditrk:name():find(padfdbk_name) then
+                        -- parse comments for bank
+                        b = parseComments(miditrk:comment())
+                        -- make bank change on tagged tracks
+                        if b:find("BANK") then
+                            if debug then
+                                print("Selected MIDI Track: " .. miditrk:name() .. " Bank tag: " .. b)
+                            end
+                            -- if debug then print("Selected MIDI Track output port: " .. miditrk:output():midi(0):name()) end
+
+                            -- get SysEx code for bank change
+                            if b == "BANK_A" or b == "BANKA" or b == "BANK A" then
+                                mpk2_bank = 0x00
+                            elseif b == "BANK_B" or b == "BANKB" or b == "BANK B" then
+                                mpk2_bank = 0x01
+                            elseif b == "BANK_C" or b == "BANKC" or b == "BANK C" then
+                                mpk2_bank = 0x02
+                            elseif b == "BANK_D" or b == "BANKD" or b == "BANK D" then
+                                mpk2_bank = 0x03
+                            else -- default to Bank A
+                                mpk2_bank = 0x00
+                            end
+
+                            -- create SysEx message for bank change
+                            local syx = string.char(0xf0, 0x47, 0x00, mpk2_model, 0x30, 0x00, 0x04, 0x01, 0x00, 0x18,
+                                mpk2_bank, 0xf7)
+                            -- transmit message
+                            tx_midi(syx, #syx, false)
+
+                            -- disconnect here for single selection
+                            if not multi_sel then
+                                -- disconnect old
+                                padfdbk:input():midi(0):disconnect_all()
+                            end
+                            -- connect output port for feedback
+                            padfdbk:input():midi(0):connect(miditrk:output():midi(0):name())
+                        else
+                            padfdbk:input():midi(0):disconnect_all()
+                        end
+                    end
+                end
+            end
+            ::skip::
+        end
+
+        function selectTrack()
+            -- get current selection in editor
+            sel = Editor:get_selection()
+
+            -- check for track(s) selected
+            if sel.tracks:routelist():size() == 1 then
+                multi_sel = false
+                configFeedback()
+            elseif sel.tracks:routelist():size() > 1 then
+                -- get selected tracks
+                local t_tbl = {}
+
+                for t in Session:get_tracks():iter() do
+                    local trk = t:to_track():to_midi_track()
+                    if not trk:isnil() and trk:is_selected() and not trk:name():find(padfdbk_name) then
+                        table.insert(t_tbl, trk)
+                    end
+                end
+
+                -- parse comments to see if selections belong to the same bank
+                local falsecnt = 0
+                for i = 1, #t_tbl do
+                    if i > 1 then
+                        if #t_tbl[i - 1]:comment() > 0 and #t_tbl[i]:comment() > 0 then
+                            local s1 = parseComments(t_tbl[i - 1]:comment())
+                            local s2 = parseComments(t_tbl[i]:comment())
+                            if s2:find("BANK") and s1:find("BANK") then
+                                local b = (s2 == s1)
+                                if not b then
+                                    falsecnt = falsecnt + 1
+                                end
+                            end
+                        end
+                    end
+                end
+
+                -- clear track table
+                for i = 1, #t_tbl do
+                    t_tbl[i] = nil
+                end
+                t_tbl = nil
+
+                -- config for multi tracks of same bank
+                if falsecnt == 0 then
+                    multi_sel = true
+                    configFeedback()
+                else
+                    if debug then
+                        print("Multiple selections do not share the same bank. Last track selected in use.")
+                    end
+                    multi_sel = false
+                    configFeedback()
+                end
+            end
+        end
+
+        -- wait for editor change signal (selections)
+        if (signal == LuaSignal.Change) then
+            if configured then
+                selectTrack()
+            else
+                chkConfig()
+            end
+        end
+    end
+end
---
+++ mpk2_config.lua
@@ -0,0 +1,191 @@
+ardour {
+    ["type"] = "EditorAction",
+    name = "MPK2 Auto Feedback Config",
+    license = "MIT",
+    author = "sollapse",
+    description = "Configure MPK2 Auto Feedback. \n\nMPK2 series SysEx information provided by Nick Smith @ https://github.com/nsmith-/mpk2"
+}
+
+function factory()
+    padfdbk_name = ""
+    mpk2_out = ""
+    mpk2_model = ""
+    mpk2_padchn = ""
+
+    return function()
+
+        -- find all MIDI hardware that accepts input	
+        function deviceList()
+            _, v = Session:engine():get_backend_ports("", ARDOUR.DataType("midi"),
+                ARDOUR.PortFlags.IsInput | ARDOUR.PortFlags.IsPhysical, C.StringVector())
+            p = {}
+            d = "MIDI Hardware Devices For Output"
+            print(d)
+            print(string.rep("-", #d + 16))
+            for n in v[4]:iter() do
+                p[Session:engine():get_pretty_name_by_name(n)] = n
+                print(n)
+            end
+            return p
+        end
+
+        function getModel()
+            if mpk2_out:find("225") then
+                mpk2_model = "23"
+            elseif mpk2_out:find("249") then
+                mpk2_model = "24"
+            elseif mpk2_out:find("261") then
+                mpk2_model = "25"
+            end
+        end
+
+        -- open or create config file in session directory
+        -- CSV format
+        -- line 1: "MPK2 device port","MPK2 device code","pad channel","feedback track name" 
+        cfgname = ARDOUR.LuaAPI.build_filename(Session:path(), "mpk2.cfg")
+
+        if ARDOUR.LuaAPI.file_test(cfgname, ARDOUR.LuaAPI.FileTest.Exists) then
+            cfgfile = io.open(cfgname, "r+")
+            io.input(cfgfile)
+
+            -- load variables
+            local r = io.read()
+
+            local vars = {}
+            for v in r:gmatch("([^,]+)") do
+                table.insert(vars, v)
+                print(v)
+            end
+
+            if #vars == 4 then
+                mpk2_out = vars[1]
+                mpk2_model = vars[2]
+                mpk2_padchn = vars[3]
+                padfdbk_name = vars[4]
+                allvars = true
+            else
+                allvars = false
+            end
+
+        else
+            -- create file
+            cfgfile = io.open(cfgname, "w+")
+            allvars = false
+        end
+
+        -- create correct dialog
+        if allvars then
+            dialog_options = {{
+                type = "entry",
+                key = "fdbktrack",
+                default = padfdbk_name,
+                title = "Pad Feedback Track Name"
+            }, {
+                type = "checkbox",
+                key = "hidetrack",
+                default = false,
+                title = "Hide Feedback Track?"
+            }, {
+                type = "dropdown",
+                key = "port",
+                default = Session:engine():get_pretty_name_by_name(mpk2_out),
+                title = "Select MPK2 Device",
+                values = deviceList()
+            }, {
+                type = "entry",
+                key = "padchn",
+                default = mpk2_padchn,
+                title = "Pad Channel"
+            }}
+        else
+            -- use default values
+            dialog_options = {{
+                type = "entry",
+                key = "fdbktrack",
+                default = "MPK2 Pad Fdbk",
+                title = "Pad Feedback Track Name"
+            }, {
+                type = "checkbox",
+                key = "hidetrack",
+                default = false,
+                title = "Hide Feedback Track?"
+            }, {
+                type = "dropdown",
+                key = "port",
+                title = "Select MPK2 Device",
+                values = deviceList()
+            }, {
+                type = "entry",
+                key = "padchn",
+                default = "10",
+                title = "Pad Channel"
+            }}
+        end
+
+        -- run main dialog
+        local rv = LuaDialog.Dialog("MPK2 Auto Feedback Config", dialog_options):run()
+        dialog_options = nil -- drop references 
+        collectgarbage() -- release the references immediately
+
+        -- dialog cancelled
+        if not rv then
+            io.close(cfgfile)
+            return
+        end
+
+        -- populate vars
+        mpk2_out = rv["port"]
+        mpk2_padchn = rv["padchn"]
+        padfdbk_name = rv["fdbktrack"]
+
+        -- find MPK2 model code from device name
+        getModel()
+
+        -- write values to cfg file
+        io.output(cfgfile)
+        cfgvars = mpk2_out .. "," .. mpk2_model .. "," .. mpk2_padchn .. "," .. padfdbk_name
+        cfgfile:seek("set", 0)
+        io.write(cfgvars .. "\n")
+        io.close(cfgfile)
+
+        -- test for feedback track
+        t = Session:route_by_name(padfdbk_name)
+        if t:isnil() then
+            -- create new feedback track
+            padfdbk = Session:new_midi_track(ARDOUR.ChanCount(ARDOUR.DataType("midi"), 1),
+                ARDOUR.ChanCount(ARDOUR.DataType("midi"), 1), true, ARDOUR.PluginInfo(), nil, nil, 1, padfdbk_name,
+                ARDOUR.PresentationInfo.max_order, ARDOUR.TrackMode.Normal, false)
+
+            if not padfdbk:front():isnil() then
+                padfdbk:front():set_comment("[PLAY]", nil)
+                padfdbk:front():set_capture_channel_mode(ARDOUR.ChannelMode.ForceChannel, 1 <<
+                    tonumber(string.format("%x", tonumber(mpk2_padchn) - 1), 16))
+                padfdbk:front():output():midi(0):connect(mpk2_out)
+            end
+        end
+
+        tv = Editor:rtav_from_route(Session:route_by_name(padfdbk_name)):to_timeaxisview()
+        if rv["hidetrack"] then
+            Editor:hide_track_in_display(tv, true)
+        else
+            Editor:show_track_in_display(tv, true)
+        end
+
+    end
+end
+
+function icon(params)
+    return function(ctx, width, height, fg)
+        local txt = Cairo.PangoLayout(ctx, "ArdourMono " .. math.ceil(height / 3) .. "px")
+        ctx:set_source_rgba(1, 1, 1, 1)
+        txt:set_text("MPK")
+        local tw, th = txt:get_pixel_size()
+        ctx:move_to(.3 * (width - tw), .5 * (height - th))
+        txt:show_in_cairo_context(ctx)
+        ctx:set_source_rgba(1, 0, 0, 1)
+        txt:set_text("2")
+        local tw, th = txt:get_pixel_size()
+        ctx:move_to(.9 * (width - tw), .5 * (height - th))
+        txt:show_in_cairo_context(ctx)
+    end
+end
